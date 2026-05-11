1040491276ea18fe01e4146564b94884e58865b9
Тимур Бикмухаметов <tim.bic22@gmail.com>
2026-05-11 23:29:15 +0300

    styles rework + assets update

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,3 +1,25 @@
-declare global {}
+import type { AsTag } from "reka-ui";
+
+declare global {
+	type AsPropDefault = AsTag | Component;
+
+	interface IconProps extends HTMLAttributes {
+		name: string;
+		mode?: "svg" | "css";
+		size?: string | number;
+		customize?: (content: string, name?: string, prefix?: string, provider?: string) => string;
+	}
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Class = any;
+export interface StyledComponentProps {
+	class?: Class;
+	unstyled?: boolean;
+}
+export interface PrimitiveComponentProps {
+	as?: AsPropDefault;
+	asChild?: boolean;
+}
+export interface StyledPrimitiveComponentProps extends StyledComponentProps, PrimitiveComponentProps {}
 
 export {};
