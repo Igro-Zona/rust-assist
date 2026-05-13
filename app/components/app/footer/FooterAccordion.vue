6dc893135ac1b4edeae5f2ed260a08f61165eaee
Тимур Бикмухаметов <tim.bic22@gmail.com>
2026-05-13 05:30:07 +0300

    footer a11y improvements

--- app/components/app/footer/FooterAccordion.vue
+++ app/components/app/footer/FooterAccordion.vue
@@ -12,7 +12,9 @@
 			:value="String(index)"
 		>
 			<AccordionHeader as="h3">
-				<AccordionTrigger class="flex w-full min-w-0 cursor-pointer items-center justify-between gap-4 py-2.5">
+				<AccordionTrigger
+					class="flex w-full min-w-0 cursor-pointer items-center justify-between gap-4 py-2.5 transition-[padding] data-[state=open]:pb-0"
+				>
 					<span class="truncate text-sm leading-1 font-semibold uppercase">
 						{{ item.title }}
 					</span>
@@ -27,16 +29,16 @@
 
 			<AccordionContent
 				v-if="item.links"
-				as="ul"
-				class="space-y-1.5 overflow-hidden data-[state=closed]:animate-[accordion-animation-up_200ms_ease-out] data-[state=open]:animate-[accordion-animation-down_200ms_ease-out]"
+				class="overflow-hidden data-[state=closed]:animate-[accordion-animation-up_200ms_ease-out] data-[state=open]:animate-[accordion-animation-down_200ms_ease-out]"
 			>
-				<li
-					v-for="(link, idx) in item.links"
-					:key="idx"
-					class="text-toned pl-2.5 text-xs leading-2 font-medium last:pb-2.5"
-				>
-					<UiLink v-bind="link" />
-				</li>
+				<ul class="text-toned space-y-1.5 py-2.5 pl-2.5 text-xs leading-2 font-medium">
+					<li
+						v-for="(link, idx) in item.links"
+						:key="idx"
+					>
+						<UiLink v-bind="link" />
+					</li>
+				</ul>
 			</AccordionContent>
 		</AccordionItem>
 	</AccordionRoot>
