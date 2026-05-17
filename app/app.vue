ea525dd28139b1445e03f9c0f72e87f5c76705ea
Тимур Бикмухаметов <tim.bic22@gmail.com>
2026-05-17 18:25:00 +0300

    markup update

--- app/app.vue
+++ app/app.vue
@@ -2,13 +2,12 @@
 	<UiContainer>
 		<UiLogo />
 	</UiContainer>
-	<UiContainer class="flex flex-col gap-15 py-15">
+	<UiContainer class="mt-200 flex flex-col gap-15 py-15">
+		<RustStats />
+		<RustSkins />
 		<RustItems />
-
 		<RustServers />
-
 		<RustReviews />
-
 		<RustFAQ />
 	</UiContainer>
 
